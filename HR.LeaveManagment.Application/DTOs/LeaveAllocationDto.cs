a757372898e81c17548ad5c1a9b7f906e5b791f8
julian190 <68065877+julian190@users.noreply.github.com>
2023-12-16 12:00:15 +1300

    Add project files.

---
+++ HR.LeaveManagment.Application/DTOs/LeaveAllocationDto.cs
@@ -0,0 +1,18 @@
+﻿using HR.LeaveManagment.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagment.Application.DTOs
+{
+    public class LeaveAllocationDto
+    {
+        public int NumberOfDays { get; set; }
+        public LeaveTypeDto LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+        public string EmployeeId { get; set; }
+    }
+}
---
+++ HR.LeaveManagment.Application/DTOs/LeaveRequest/LeaveRequestListDto.cs
@@ -0,0 +1,16 @@
+﻿using HR.LeaveManagment.Application.DTOs.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagment.Application.DTOs.LeaveRequest
+{
+    internal class LeaveRequestListDto:BaseDto
+    {
+        public LeaveTypeDto LeaveType { get; set; }
+        public DateTime DateRequested { get; set; }
+        public bool? Approved { get; set; }
+    }
+}
